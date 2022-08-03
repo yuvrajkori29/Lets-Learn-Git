74882cacc7be7100e8b9451acb7bf27164f43d29
Yuvraj Kori <yuvrajkori29@gmail.com>
2022-08-03 21:46:36 +0530

    vo stree h -- added

--- first.cpp
+++ first.cpp
@@ -5,8 +5,8 @@ int main()
 {
     int a=1,b=2;
 
-    string str="abc",st="xyz";
-    cout<<str+st<<endl;
+    string str="abc",st="xyz",stri="vo stree h vo kuch bhi kr skti h ";
+    cout<<str+st+stri<<endl;
 
     cout<<a+b;
     cout<<a<<b;
